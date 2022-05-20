0b7a355ed51380d07835d5648826c32dc11b9417
Shivaa Krishnan <shivainfotech12@gmail.com>
2022-05-20 20:41:05 +0530

    Tutorial Video 2 completed - Firebase Auth and UI improvements added

--- app/src/main/java/com/shiva/learning/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/shiva/learning/kotlinmessenger/LoginActivity.kt
@@ -2,10 +2,54 @@ package com.shiva.learning.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.shiva.learning.kotlinmessenger.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+        binding.txtBack.setOnClickListener{
+            finish()
+        }
+
+        binding.btnLogin.setOnClickListener { performLogin() }
+    }
+
+    private fun performLogin() {
+        val email = binding.txtEmail.text.toString()
+        val password = binding.txtPassword.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    Toast.makeText(baseContext, "Authentication Successful.",
+                        Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                   // updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
     }
 }
--- app/src/main/java/com/shiva/learning/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/shiva/learning/kotlinmessenger/MainActivity.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.shiva.learning.kotlinmessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +24,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener{
-            showToast("Muruga")
+            performRegister()
         }
 
         binding.txtAccountLogin.setOnClickListener{
@@ -26,6 +35,33 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val username = binding.txtUsername.text
+        val password: String = binding.txtPassword.text.toString()
+        val email: String = binding.txtEmail.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            showToast("Username or password is empty")
+            return
+        }
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", it.exception)
+                    Toast.makeText(baseContext, "Create user failed. ${it.exception?.message}",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+
+            }
     }
 
     private fun showToast(message: String) {
