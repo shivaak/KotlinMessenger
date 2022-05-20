bc0b25a09255408bcc3ce2c96bdbf213f3f2e1f1
Shivaa Krishnan <shivainfotech12@gmail.com>
2022-05-20 17:06:38 +0530

    Tutorial Video 1 completed - Basic UI for Login screen

--- app/src/main/java/com/shiva/learning/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/shiva/learning/kotlinmessenger/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.shiva.learning.kotlinmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.shiva.learning.kotlinmessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener{
+            showToast("Muruga")
+        }
+
+        binding.txtAccountLogin.setOnClickListener{
+            Log.d("MainActivity","Already having account link clicked")
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
